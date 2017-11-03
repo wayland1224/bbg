cc65f5ef372b6ab5b16950c80fcd0d5f17f29d01
chenwenlong <chenwenlong2955@4399inc.com>
2017-11-03 10:44:38 +0800

    debug

--- application/config.php
+++ application/config.php
@@ -17,7 +17,7 @@ return [
     // 应用命名空间
     'app_namespace'          => 'app',
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => true,
     // 应用Trace
     'app_trace'              => false,
     // 应用模式状态
@@ -56,7 +56,7 @@ return [
     // +----------------------------------------------------------------------
 
     // 默认模块名
-    'default_module'         => 'index',
+    'default_module'         => 'm',
     // 禁止访问模块
     'deny_module_list'       => ['common'],
     // 默认控制器名
